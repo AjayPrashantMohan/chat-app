2d68b1e598298d702efe552d447daa09b5164584
Ajay Mohan <ajay.mohan@intellectdesign.com>
2017-06-21 15:57:57 +0530

    Add feature to display if the user disconnects from the chat group

--- server.js
+++ server.js
@@ -12,9 +12,24 @@ var clientInfo = {};
 io.on('connection', function(socket) {
 	console.log('User connected via socket.io!');
 
-	socket.on('joinRoom', function(req) {
-		clientInfo[socket.id] = req;//socket.id is a dynamic id created by socket
-		socket.join(req.room); //joins to the reqired room
+	socket.on('disconnect', function() { /*disconnect is socket's built in event*/
+		var userData = clientInfo[socket.id];
+
+		if (typeof userData !== 'undefined') {
+			socket.leave(userData.room); //leaves the room ,where socket.leave is a socket's built in function
+			io.to(userData.room).emit('message', {
+				name: 'System',
+				text: userData.name + ' has left the chat!',
+				timestamp: moment().valueOf()
+			});
+
+			delete clientInfo[socket.id];
+		}
+	});
+
+	socket.on('joinRoom', function(req) { /*joinRoom is an custom event*/
+		clientInfo[socket.id] = req; //socket.id is a dynamic id created by socket
+		socket.join(req.room); //joins to the required room, where socket.join is a socket's built in function
 		socket.broadcast.to(req.room).emit('message', { //send message that user has joined to other members of the room
 			name: 'System',
 			text: req.name + ' has joined the chat!',
@@ -22,11 +37,11 @@ io.on('connection', function(socket) {
 		});
 	});
 
-	socket.on('message', function(message) {
+	socket.on('message', function(message) { /*message is an custom event*/
 		console.log('Message received: ' + message.text);
 		message.timestamp = moment().valueOf(); //moment().valueOf() gives timestamp in milliseconds
 		io.to(clientInfo[socket.id].room).emit('message', message); //send message to everybody in group
-		
+
 		/*io.emit('message', message);*/ //send message to everybody
 		/*socket.broadcast.emit('message',message);*/ //send message to everybody except the one who sent it
 	});
