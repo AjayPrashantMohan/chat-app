32628456ba6d1322650f83f393c490058bb5947b
Ajay Mohan <ajay.mohan@intellectdesign.com>
2017-06-21 14:47:05 +0530

    Add name to the chat messages

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,27 @@
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
 var socket = io();
 
-socket.on('connect',function(){
+socket.on('connect', function() {
 	console.log('connected to socket.io server!');
 });
 
-socket.on('message',function(message){
+socket.on('message', function(message) {
 	var momentTimeStamp = moment.utc(message.timestamp);
-	$('.messages').append('<p><strong>'+momentTimeStamp.local().format('h:mm a')+': </strong>'+message.text+'</p>');// .local() will convert timestamp according to the local time zone
+	var $message = $('.messages');
+
+	$message.append('<p><strong>'+message.name+' '+momentTimeStamp.local().format('h:mm a') +'</strong></p>');
+	$message.append('<p>'+message.text+'</p>')
 });
 
 var $form = $('#message-form');
 
-$form.on('submit',function(event){
+$form.on('submit', function(event) {
 	event.preventDefault();
 	var $message = $form.find('input[name=message]');
-	socket.emit('message',{
-		text:$message.val()
+	socket.emit('message', {
+		name: name,
+		text: $message.val()
 	});
 	$message.val('');
 });
