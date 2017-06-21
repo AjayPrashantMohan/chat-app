0a181c2b7712f105cfd0a79fdc2a781db3a35c5d
Ajay Mohan <ajay.mohan@intellectdesign.com>
2017-06-21 16:24:09 +0530

    added @currentUsers command

--- server.js
+++ server.js
@@ -9,9 +9,31 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+/*Sends the current users in the sockets*/
+function sendCurrentUsers(socket) {
+	var info = clientInfo[socket.id];
+	var users = [];
+
+	if (typeof info === 'undefined') {
+		return;
+	}
+
+	Object.keys(clientInfo).forEach(function(socketId) {
+		var userInfo = clientInfo[socketId];
+		if (info.room == userInfo.room) {
+			users.push(userInfo.name);
+		}
+	});
+	socket.emit('message', {
+		name: 'System',
+		text: 'Current Users: ' + users.join(', '),
+		timestamp: moment().valueOf()
+	})
+}
+
 io.on('connection', function(socket) {
 	console.log('User connected via socket.io!');
-
+	/*while disconnecting from group or room*/
 	socket.on('disconnect', function() { /*disconnect is socket's built in event*/
 		var userData = clientInfo[socket.id];
 
@@ -27,6 +49,7 @@ io.on('connection', function(socket) {
 		}
 	});
 
+	/*while joining to a group or room*/
 	socket.on('joinRoom', function(req) { /*joinRoom is an custom event*/
 		clientInfo[socket.id] = req; //socket.id is a dynamic id created by socket
 		socket.join(req.room); //joins to the required room, where socket.join is a socket's built in function
@@ -39,11 +62,15 @@ io.on('connection', function(socket) {
 
 	socket.on('message', function(message) { /*message is an custom event*/
 		console.log('Message received: ' + message.text);
-		message.timestamp = moment().valueOf(); //moment().valueOf() gives timestamp in milliseconds
-		io.to(clientInfo[socket.id].room).emit('message', message); //send message to everybody in group
 
-		/*io.emit('message', message);*/ //send message to everybody
-		/*socket.broadcast.emit('message',message);*/ //send message to everybody except the one who sent it
+		if (message.text == '@currentUsers') {
+			sendCurrentUsers(socket);
+		} else {
+			message.timestamp = moment().valueOf(); //moment().valueOf() gives timestamp in milliseconds
+			io.to(clientInfo[socket.id].room).emit('message', message); //send message to everybody in group
+			/*io.emit('message', message);*/ //send message to everybody
+			/*socket.broadcast.emit('message',message);*/ //send message to everybody except the one who sent it
+		}
 	});
 
 	socket.emit('message', {
